5ec3eee0d850fe030c152e49004fbb0e6fc992c2
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-03-10 17:32:08 +0200

    add pulse loading

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -6,7 +6,8 @@ function PortfolioItem({ img, link, title, date }) {
   return (
     <Link href={link}>
       <a target="_blank" rel="noreferrer" className="h-full w-full">
-        <div className="relative mb-8 h-96 w-full rounded-3xl bg-neutral-800">
+        <div className="relative mb-8 h-96 w-full rounded-3xl">
+          <div className="w-full h-full bg-neutral-800 animate-pulse rounded-3xl"></div>
           <Image
             src={img}
             layout="fill"
