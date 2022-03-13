3325ebfc77b6a71ab5b1e04612fd74408baff152
eemeli ruohomäki <56453212+eeme1i@users.noreply.github.com>
2022-03-13 19:12:08 +0200

    change portfolioitem

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -11,28 +11,28 @@ function PortfolioItem({ img, link, title, date }) {
       viewport={{ once: true }}
       transition={{ delay: 0.2 * 0.3 }}
     >
-      <Link href={link}>
-        <a target="_blank" rel="noreferrer" className="h-full w-full">
-          <div className="relative mb-8 h-96 w-full rounded-lg">
-            <div className="w-full h-full bg-neutral-800 absolute animate-pulse rounded-lg"></div>
-            <Image
-              src={img}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-lg"
-            />
-            <div className="opacity-0 hover:opacity-100 absolute h-full w-full bg-neutral-900/[0.6] transition-all duration-150 ease-linear">
-              <div className="flex justify-between pt-7 px-8">
+      <div className="relative mb-8 h-96 w-full rounded-lg">
+        <div className="w-full h-full bg-neutral-800 absolute animate-pulse rounded-lg"></div>
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg"
+        />
+        <div className="opacity-0 hover:opacity-100 absolute h-full w-full bg-neutral-900/[0.6] transition-all duration-150 ease-linear">
+          <div className="flex justify-between pt-7 px-8">
+            <Link href={link}>
+              <a target="_blank" rel="noreferrer" className="h-full w-full">
                 <div className="flex">
                   <p className="">{title}</p>
                   <Arrow />
                 </div>
-                <p>{date}</p>
-              </div>
-            </div>
+              </a>
+            </Link>
+            <p>{date}</p>
           </div>
-        </a>
-      </Link>
+        </div>
+      </div>
     </motion.div>
   );
 }
